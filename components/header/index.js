a1695068ecf2c49b36ba4fd12723cf957841defd
AzamatUzakov <azamatakmal053@gmail.com>
2023-12-01 21:21:04 +0500

    начал делать верстку headera на js

--- components/header/index.js
+++ components/header/index.js
@@ -1,4 +1,4 @@
-let close_modal = document.querySelector('.close')
+/* let close_modal = document.querySelector('.close')
 let menu = document.querySelector('.menu')
 let burger_menu = document.querySelector('.burger_menu')
 
@@ -15,4 +15,48 @@ close_modal.onclick = () => {
 
 burger_menu.onclick = () => {
     menu.style.display = "block"
-}
+} */
+
+let first_option_contener = document.querySelector('div')
+let icon_box = document.querySelector('div')
+
+let cinem_of_title_flex_case = document.querySelector('div')
+let cinem_of_title_flex_case_cinema_img = document.querySelector('img')
+let cinem_of_title_flex_case_kino_img = document.querySelector('img')
+
+let application_flex_case = document.querySelector('div')
+let application_flex_case_vk = document.querySelector('img')
+let application_flex_case_inst = document.querySelector('img')
+let application_flex_case_face = document.querySelector('img')
+let application_flex_case_twit = document.querySelector('img')
+
+let hot_hrefs_case = document.querySelector('div')
+let hot_hrefs_case_a_one = document.querySelector('a')
+let hot_hrefs_case_a_two= document.querySelector('a')
+let hot_hrefs_case_a_three = document.querySelector('a')
+let hot_hrefs_case_a_foor = document.querySelector('a')
+let hot_hrefs_case_a_five = document.querySelector('a')
+let hot_hrefs_case_a_six = document.querySelector('a')
+let hot_hrefs_case_a_seven = document.querySelector('a')
+
+/* 
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div')
+let first_option_contener = document.querySelector('div') */
