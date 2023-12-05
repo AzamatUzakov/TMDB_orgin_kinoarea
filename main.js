331aa3887b105e84795518372a36ec8f5a0ca093
AzamatUzakov <azamatakmal053@gmail.com>
2023-12-05 17:58:29 +0500

    делаю фильтрацию

---
+++ main.js
@@ -0,0 +1,13 @@
+/* // import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+var swiper = new Swiper(".mySwiper", {
+    
+});
+ */
