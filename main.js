28c4b0a2798b3d8a6725cd290bc52e303d8fcd3f
AzamatUzakov <azamatakmal053@gmail.com>
2023-12-06 19:47:12 +0500

    сделал переходы в фильтрации

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-/* // import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 
-var swiper = new Swiper(".mySwiper", {
-    
+
+let fil_titl = document.querySelectorAll('.hrefs')
+
+
+fil_titl.forEach(btn => {
+  btn.onclick = () => {
+
+    fil_titl.forEach(btn => btn.classList.remove('active'))
+    btn.classList.add('active')
+  }
 });
- */
-
-
-let fil_titl = document.querySelectorAll('#fil_titl')
-
-
-//fil_titl.forEach = (btn => {
-//  btn.onclick = () => {
-//
-//    //  fil_titl.forEach(btn => btn.classList.remove('active'))
-//    console.log("btn");
-//  }
-//});
 
