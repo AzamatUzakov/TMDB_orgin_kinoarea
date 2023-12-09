5f65918b2c9ff8e2100c347df96a4d1658b355fe
AzamatUzakov <azamatakmal053@gmail.com>
2023-12-09 20:38:45 +0500

    сделал выподаюшие окно в фильтрации по категориям

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 
 
 let fil_titl = document.querySelectorAll('.hrefs')
-
+let burger_menu = document.querySelector('.title img')
+let menu = document.querySelector('.menu')
+let close_img = document.querySelector('.close')
 
 fil_titl.forEach(btn => {
   btn.onclick = () => {
@@ -11,4 +13,12 @@ fil_titl.forEach(btn => {
     btn.classList.add('active')
   }
 });
+
+
+burger_menu.onclick = () => {
+  menu.style.display = "block"
+}
+close_img.onclick = () => {
+  menu.style.display = "none"
+}
 
